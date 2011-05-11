a52c1b2ea6bec858fc47f1e50c699e9279288075
wgao <wgao@172.16.4.9>
2011-05-11 14:04:39 -0700

    Mobicents bug: default application session timeout unit is minute

--- mcs-sip/src/com/mycallstation/sip/servlet/package-info.java
+++ mcs-sip/src/com/mycallstation/sip/servlet/package-info.java
@@ -1,4 +1,4 @@
-@SipApplication(name = "com.mycallstation.CallCenter", displayName = "My Call Station", mainServlet = "CallCenterServlet", sessionTimeout = 3600)
+@SipApplication(name = "com.mycallstation.CallCenter", displayName = "My Call Station", mainServlet = "CallCenterServlet", sessionTimeout = 3)
 package com.mycallstation.sip.servlet;
 
 import javax.servlet.sip.annotation.SipApplication;
